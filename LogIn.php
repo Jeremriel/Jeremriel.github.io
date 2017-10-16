092220cb88b6ee7615ba3d0b6b710dfb902575f4
Jeremriel <32828650+Jeremriel@users.noreply.github.com>
2017-10-16 14:14:43 +0900

    Add files via upload

---
+++ LogIn.php
@@ -0,0 +1,39 @@
+<?php
+	session_start();
+	if (isset($_POST['submitLogin'])){
+		$_SESSION['username']=$username=$_POST['username'];
+		$_SESSION['password']=$password=$_POST['password'];
+
+		$conn=mysqli_connect("localhost","root","","id3274236_project");
+		$result=mysqli_query($conn,"SELECT * FROM account WHERE username='$username' and password='$password'");
+		$data=mysqli_fetch_assoc($result);
+		mysqli_close($conn);
+		if ($data) header("location:VolunteerHomepage.html");
+		else $error="Sorry! Username or Password Incorrect.";
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<center>
+<head>
+	<title>Log-In</title>
+	<link rel="stylesheet" type="text/css" href="Style.css">
+</head>
+<body>
+	<div id='parent'>
+		<?php
+			if (isset($error)) echo $error;
+		?>
+		<form method="POST">
+			Username: <input type="text" name="username" placeholder="Username"> <br>
+			Password: <input type="password" name="password" placeholder="Password">
+			<input type="submit" name="submitLogin" value="Log In">
+		</form>
+		<br><span>Don't have an account? <a href="Register.php">Volunteer now!!!</a></span></br>
+		<br><a href="Homepage.html">Go back</a>
+	</div>
+	</center>
+</body>
+</body>
+</html>
---
+++ Register.php
@@ -0,0 +1,44 @@
+<?php
+	session_start();
+
+	if (isset($_POST['submitRegister'])){
+		$_SESSION['username']=$username=$_POST['username'];
+		$_SESSION['password']=$password=$_POST['password'];
+
+		$conn=mysqli_connect("localhost","root","","id3274236_project");
+		$istaken=!mysqli_query($conn,"INSERT INTO account VALUES('$username','$password')");
+		mysqli_close($conn);
+		if (!$istaken){
+			header('location:LogIn.php');
+		}
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<center>
+<head>
+	<title>Registration</title>
+	<link rel="stylesheet" type="text/css" href="Style.css">
+</head>
+<body>
+	Register your desired Username and Password for protection.</br></br>
+	
+	<i>
+	<div id='parent'>
+	<?php
+		if (isset($istaken) and $istaken) echo"Sorry this account is already taken.";
+	?>
+	</i>
+	
+	<form method="POST">
+		<h2> UserName </h2><input type="text" name="username" placeholder="Username" required > <p>
+		<h2> Password </h2><input type="password" name="password" placeholder="Password" required >
+		<input type="submit" name="submitRegister" value="Sign-Up"><br>
+		<a href="LogIn.php">Go back</a>
+	</form>
+	</center>
+	</div>
+</body>
+</html>
